10d685b4fcd679caf0b4ca19f3bc424d9d302f3d
Leovambarii <ksprp121@gmail.com>
2023-05-08 22:32:43 +0200

    Database tweaks

--- food_recipes/recipe_db.sql
+++ food_recipes/recipe_db.sql
@@ -5,54 +5,54 @@ DROP TABLE IF EXISTS `Recipe`;
 DROP TABLE IF EXISTS `ArgumentInput`;
 
 CREATE TABLE `ArgumentInput`(
-    `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Index` int UNSIGNED NOT NULL AUTO_INCREMENT,
     `IngredientText` text NOT NULL,
     `RecipesNumber` int NOT NULL,
-    PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`Index`)
+);
 
 CREATE TABLE `Recipe`(
-    `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Index` int UNSIGNED NOT NULL AUTO_INCREMENT,
     `RecipeId` int UNSIGNED NOT NULL,
     `ArgumentId` int UNSIGNED NOT NULL,
     `Title` text NOT NULL,
     `Servings` int UNSIGNED NOT NULL,
-    PRIMARY KEY (`Id`),
+    PRIMARY KEY (`Index`),
     KEY `recipe_argumentid_foreign` (`ArgumentId`),
-    CONSTRAINT `recipe_argumentid_foreign` FOREIGN KEY (`ArgumentId`) REFERENCES `ArgumentInput` (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    CONSTRAINT `recipe_argumentid_foreign` FOREIGN KEY (`ArgumentId`) REFERENCES `ArgumentInput` (`Index`)
+);
 
 CREATE TABLE `MissingIngredient`(
-    `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Index` int UNSIGNED NOT NULL AUTO_INCREMENT,
     `IngredientId` int UNSIGNED NOT NULL,
-    `RecipeId` int UNSIGNED NOT NULL,
+    `RecipeIndex` int UNSIGNED NOT NULL,
     `Name` text NOT NULL,
     `Amount` double(8, 2) NOT NULL,
     `Unit` text,
-    PRIMARY KEY (`Id`),
-    KEY `missingingredient_recipeid_foreign` (`RecipeId`),
-    CONSTRAINT `missingingredient_recipeid_frg` FOREIGN KEY (`RecipeId`) REFERENCES `Recipe` (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`Index`),
+    KEY `missingingredient_recipeid_foreign` (`RecipeIndex`),
+    CONSTRAINT `missingingredient_recipeid_frg` FOREIGN KEY (`RecipeIndex`) REFERENCES `Recipe` (`Index`)
+);
 
 CREATE TABLE `UsedIngredient`(
-    `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Index` int UNSIGNED NOT NULL AUTO_INCREMENT,
     `IngredientId` int UNSIGNED NOT NULL,
-    `RecipeId` int UNSIGNED NOT NULL,
+    `RecipeIndex` int UNSIGNED NOT NULL,
     `Name` text NOT NULL,
     `Amount` double(8, 2) NOT NULL,
     `Unit` text,
-    PRIMARY KEY (`Id`),
-    KEY `usedingredient_recipeid_foreign` (`RecipeId`),
-    CONSTRAINT `usedingredient_recipeid_frg` FOREIGN KEY (`RecipeId`) REFERENCES `Recipe` (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`Index`),
+    KEY `usedingredient_recipeid_foreign` (`RecipeIndex`),
+    CONSTRAINT `usedingredient_recipeid_frg` FOREIGN KEY (`RecipeIndex`) REFERENCES `Recipe` (`Index`)
+);
 
 CREATE TABLE `Nutrients`(
-    `Id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `RecipeId` int UNSIGNED NOT NULL,
+    `Index` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `RecipeIndex` int UNSIGNED NOT NULL,
     `Name` text NOT NULL,
     `Amount` double(8, 2) NOT NULL,
     `Unit` text NOT NULL,
-    PRIMARY KEY (`Id`),
-    KEY `nutrients_recipeid_foreign` (`RecipeId`),
-    CONSTRAINT `nutrients_recipeid_frg` FOREIGN KEY (`RecipeId`) REFERENCES `Recipe` (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`Index`),
+    KEY `nutrients_recipeid_foreign` (`RecipeIndex`),
+    CONSTRAINT `nutrients_recipeid_frg` FOREIGN KEY (`RecipeIndex`) REFERENCES `Recipe` (`Index`)
+);
